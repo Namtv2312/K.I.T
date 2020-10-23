b24aef58adec902c63fe26960fda4ec147917aaa
Nam System <Namxomcau@gmail.com>
2020-10-23 12:25:00 +0700

    turn in

--- src/main/java/Buoi6/BTVN/Bai1/DocGia.java
+++ src/main/java/Buoi6/BTVN/Bai1/DocGia.java
@@ -5,10 +5,38 @@
  */
 package Buoi6.BTVN.Bai1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author NAM SYSTEM
  */
 public class DocGia {
+    private String hoTen;
+    private int ngayLapthe;
+    private int soThangcoHieuluc;
+    
+    public void nhap()
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Nhap ho ten: ");
+        hoTen= sc.nextLine();
+        System.out.println("Nhap ngay lap the: ");
+        ngayLapthe=sc.nextInt(); 
+        System.out.println("Nhap so thang co hieu luc");
+        soThangcoHieuluc= sc.nextInt();
+    }
+
+    public DocGia() {
+    }
+
+    @Override
+    public String toString() {
+        return "DocGia{" + "hoTen=" + hoTen + ", ngayLapthe=" + ngayLapthe + ", soThangcoHieuluc=" + soThangcoHieuluc + '}';
+    }
+
+    public int getSoThangcoHieuluc() {
+        return soThangcoHieuluc;
+    }
     
 }
---
+++ src/main/java/Buoi6/BTVN/Bai2/XeOm.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Buoi6.BTVN.Bai2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author NAM SYSTEM
+ */
+public class XeOm extends NhanVien{
+    private int soKm;
+    public int  tinhtienluong()
+     {
+         return soKm*10000;
+     }
+         
+     void nhap()
+     {
+         super.nhap();
+         Scanner sc= new Scanner(System.in);
+         System.out.println("Nhap so km: ");
+         soKm = sc.nextInt();
+     }
+     void xuat()
+     {
+         System.out.println(toString());
+     }
+    
+
+    @Override
+    public String toString() {
+        return "XeOm{"+super.toString() + "soKm=" + soKm + '}';
+    }
+
+    public XeOm() {
+    }
+
+    public int getSoKm() {
+        return soKm;
+    }
+
+    public void setSoKm(int soKm) {
+        this.soKm = soKm;
+    }
+    
+     
+}
